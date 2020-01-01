aecfd8fd18697e73a635ec0d68c1c9c07e6d8126
BDMorrell <BDMorrell@gmail.com>
2019-12-31 17:36:09 -0700

    Isolate code for the main app's table

--- components/EditableTable.js
+++ components/EditableTable.js
@@ -1,35 +1,58 @@
 import React from "react";
 import { useTable } from "react-table";
-import { EditableTableFormat } from "./BasicTable";
 import CheckbookTableColumns from "./objects/CheckbookTableColumns";
-import setFailure from "./objects/setFailure";
+import styled from "styled-components";
+import { theme } from "../static/constants";
 
-// function generateHeader(columnDefinition) {
-//   return (<tr key={"header"}>
-//     {columnDefinition.map(c => <th key={c.key}>{c.displayName}</th>)}
-//   </tr>);
-// }
-
-// function generateRow(columnDefinition, row) {
-//   return columnDefinition.map(column => (
-//     <td key={column.key}>
-//       {setFailure(column.formatter), "ERROR", row[column.key]}
-//     </td>
-//   ));
-// }
-
-// function generateTableBody(tableContents, columnDefinition) {
-//   return tableContents.map((row, i) => (<tr key={i}>{generateRow(columnDefinition, row)}</tr>));
-// }
-
-function renderCellContents({cell})
-{
-  try {
-    return cell.column.formatter(cell.value);
-  } catch(e) {
-    console.info(e); // info so it can be filtered out, because the table has a lot of cells
-    return <span style={{ color: "red" }}>E.{e.name}</span>
+const EditableTableFormat = styled.table`
+  width: 100%;
+  table-layout: auto;
+  border-collapse: collapse;
+  thead th {
+    position: sticky;
+    top: 0;
+    background-color: white;
+    box-shadow: 0 1px ${theme.lightColor}; // bottom border when sticked
+    border-top: none;
   }
+  * tr {
+    height: 2em;
+    td, th {
+      border: 1px solid ${theme.lightColor};
+      padding: .25em;
+    }
+    * {
+      :first-child {
+        border-left: none;
+      }
+      :last-child {
+        border-right: none;
+      }
+    }
+  }
+  caption { // for screen readers only
+    border-top: 80vh solid transparent;
+    caption-side: bottom;
+    color: transparent;
+    user-select: none;
+  }
+`;
+
+function renderCellContents({ cell }) {
+  try {
+    return (cell.column.formatter(cell.value));
+  } catch (e) {
+    console.info(e); // info so it can be filtered out, because the table has a lot of cells
+    return (<span style={{ color: "red" }}>E.{e.name}</span>);
+  }
+}
+
+function renderCell(tdInfo) {
+  return (
+    <td {...tdInfo.getCellProps()}>
+      {tdInfo.render(renderCellContents)}
+    </td>
+  );
 }
 
 export default function EditableTable({ contents, ...props }) {
@@ -60,13 +83,7 @@ export default function EditableTable({ contents, ...props }) {
           prepareRow(trInfo);
           return (
             <tr {...trInfo.getRowProps()}>
-              {
-                trInfo.cells.map(tdInfo => (
-                  <td {...tdInfo.getCellProps()}>
-                    {tdInfo.render(renderCellContents)}
-                  </td>
-                ))
-              }
+              {trInfo.cells.map(renderCell)}
             </tr>
           );
         })}
